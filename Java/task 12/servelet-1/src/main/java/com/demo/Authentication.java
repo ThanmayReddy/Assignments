2802057db92128139c672efcd4dfbfcaf6b96188
ThanmayReddy <thanmay.reddy@wavemaker.com>
2022-04-05 14:57:32 +0530

    added servelet task

---
+++ Java/task 12/servelet-1/src/main/java/com/demo/Authentication.java
@@ -0,0 +1,35 @@
+package com.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Authentication implements Filter {
+    private static final Logger logger = LoggerFactory.getLogger(Login.class);
+
+
+    public void init(FilterConfig arg0) throws ServletException {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+        logger.info("welcome to loggers");
+        PrintWriter out = resp.getWriter();
+        String user = req.getParameter("name");
+        String password = req.getParameter("password");
+        if (password.equals("admin")) {
+            logger.info("password correct");
+            chain.doFilter(req, resp);
+            logger.info("authentication done");
+        } else {
+            logger.info("incorrect credentials");
+            out.print("incorrect username or password");
+        }
+
+    }
+
+    public void destroy() {
+    }
+}
---
+++ Java/task 12/servelet-1/src/main/java/com/demo/Test.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Test {
+    public Test() {
+    }
+
+    public static void main(String[] args) {
+        String s = "abcd";
+        System.out.println(StringUtils.capitalize(s));
+    }
+}
